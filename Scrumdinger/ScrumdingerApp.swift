2a9817d2035f3e40c3f2d70e5a61d50abca22e3a
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-12-19 23:59:38 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Aaryan Kothari on 19/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
