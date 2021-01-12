094ec5c944922a0c35c651a4010770d46dfea124
Aaryan Kothari <aaryan.kothari@gmail.com>
2021-01-12 18:59:39 +0530

    Add: NavigationView

--- Scrumdinger/App/ScrumsView.swift
+++ Scrumdinger/App/ScrumsView.swift
@@ -15,16 +15,24 @@ struct ScrumsView: View {
     var body: some View {
         List {
             ForEach(scrums) { scrum in
-                CardView(scrum: scrum)
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                }
                     .listRowBackground(scrum.color)
             } // FOREACH
         } // LIST
+        .navigationTitle("Daily Scrums")
+        .navigationBarItems(trailing: Button(action: {}, label: {
+            Image(systemName: "plus")
+        }))
     } // BODY
 }
 
 // MARK:- PREVIEWS
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
+        NavigationView {
         ScrumsView(scrums: DailyScrum.data)
+        }
     }
 }
