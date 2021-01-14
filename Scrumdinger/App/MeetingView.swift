8c149159e0b1464fc898191f40133475bcbab218
Aaryan Kothari <aaryan.kothari@gmail.com>
2021-01-14 13:39:34 +0530

    update: DetailView accessibility setup

--- Scrumdinger/App/MeetingView.swift
+++ Scrumdinger/App/MeetingView.swift
@@ -8,46 +8,38 @@
 import SwiftUI
 
 struct MeetingView: View {
-    
-    // BODY
+    // MARK:- PROPERTIES
+    @Binding var scrum: DailyScrum
+    // MARK:- BODY
     var body: some View {
-        VStack {
-            ProgressView(value: 5, total: 15)
-            HStack{
-                VStack(alignment: .leading) {
-                    Text("Seconds Elapsed")
-                        .font(.caption)
-                    Label("300", systemImage: "hourglass.bottomhalf.fill")
-                } // VSTACK
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("Seconds Remaining")
-                        .font(.caption)
-                    Label("600", systemImage: "hourglass.tophalf.fill")
-                } // VSTACK
-            } // HSTACK
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel(Text("Time Remaining"))
-            .accessibilityLabel(Text("10 minutes"))
-            Circle()
-                .strokeBorder(lineWidth: 24, antialiased: true)
-            HStack {
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button(action: {}) {
-                    Image(systemName: "forward.fill")
-                }
-                .accessibilityLabel(Text("Next speaker"))
-            } // HSTACK
-        } // VSTACK
+        ZStack {
+            
+            RoundedRectangle(cornerRadius: 16)
+                .fill(scrum.color)
+            
+            VStack {
+
+                Circle()
+                    .strokeBorder(lineWidth: 24, antialiased: true)
+                HStack {
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel(Text("Next speaker"))
+                } // HSTACK
+            } // VSTACK
+        } // ZSTACK
         .padding()
+        .foregroundColor(scrum.color.accessibleFontColor)
     }
 }
 
 // PREVIEWS
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView()
+        MeetingView(scrum: .constant(DailyScrum.data[0]))
     }
 }
 
--- Scrumdinger/View/MeetingHeader.swift
+++ Scrumdinger/View/MeetingHeader.swift
@@ -8,34 +8,52 @@
 import SwiftUI
 
 struct MeetingHeader: View {
+    // MARK:- PROPERTIES
+    @Binding var secondsElapsed: Int
+    @Binding var secondsRemaining: Int
+    
+    private var progress: Double {
+        guard secondsRemaining > 0 else { return 1}
+        let totalSeconds = Double(secondsElapsed + secondsRemaining)
+        return Double(secondsElapsed) / totalSeconds
+    }
+    
+    private var minutesRemaining: Int {
+        return secondsRemaining/60
+    }
+    
+    private var minutesRemainingMetric: String {
+        return minutesRemaining == 1 ? "minute" : "minutes"
+    }
+    
     // MARK:- BODY
     var body: some View {
         VStack {
-            ProgressView(value: 5, total: 15)
+            ProgressView(value: progress)
             HStack{
                 VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
                         .font(.caption)
-                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
                 } // VSTACK
                 Spacer()
                 VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
                         .font(.caption)
-                    Label("600", systemImage: "hourglass.tophalf.fill")
+                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
                 } // VSTACK
             } // HSTACK
         } // VSTACK
         .accessibilityElement(children: .ignore)
         .accessibilityLabel(Text("Time Remaining"))
-        .accessibilityLabel(Text("10 minutes"))
+        .accessibilityLabel(Text("\(minutesRemaining) \(minutesRemainingMetric)"))
     } // BODY
 }
 
 // MARK:- PREVIEWS
 struct MeetingHeader_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingHeader()
+        MeetingHeader(secondsElapsed: .constant(300), secondsRemaining: .constant(150))
             .previewLayout(.sizeThatFits)
     }
 }
